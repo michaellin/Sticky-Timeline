e99e66da9e9806d5d95fe386937e68e78a411625
michaellin <michaelv03@gmail.com>
2012-10-03 01:27:41 -0700

    cleaning up

--- note.js
+++ note.js
@@ -1,10 +1,28 @@
-var n = 1;
-
-var new_note = function(div) {
-    var str = "<div id='test" + n++ + "' class='draggables'> <textarea class='divtext'></textarea></div>";
-    $(str).appendTo(div);
-    $(document).ready(function() {
-        $(".draggables").draggable();
-    });
-};
+$(function() {
+    var new_note = function() {
+        $('.container').append(
+        "<div id='test' class='draggables'> <textarea class='divtext'></textarea></div>");
+        $(function() {
+            $(".draggables").draggable({ snap : true });
+        });
+    };
     
+    var n = 5;
+    var progress = function() {
+        $('.bars').progressbar({
+            value: n += 5
+        })
+    };
+
+    $(".buttons").button();
+    $(".buttons").click(progress);
+
+    var new_button = function() {
+        $('.many-buttons').append(
+        "<br><button class='buttons1'><span class='button-test'> Clone Myself</span></button>");
+        $(".buttons1").button();
+        $(".buttons1").click(new_button);
+    };
+    $(".buttons1").button();
+    $(".buttons1").click(new_button);
+});
